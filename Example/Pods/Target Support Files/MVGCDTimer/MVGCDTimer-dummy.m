f94b644de1ed39bd73f48c4a47dc78f2057c0707
Maxim Vainshtein <maximv88@gmail.com>
2017-12-22 00:08:19 +0200

    Updates

---
+++ Example/Pods/Target Support Files/MVGCDTimer/MVGCDTimer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MVGCDTimer : NSObject
+@end
+@implementation PodsDummy_MVGCDTimer
+@end
---
+++ Example/Pods/Target Support Files/Pods-MVGCDTimer_Tests/Pods-MVGCDTimer_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVGCDTimer_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MVGCDTimer_Tests
+@end
